935fe1d7cfa51f551db256e3d7ff12a18378b76a
tawsinDOTuddin <tawsin.uddin@gmail.com>
2017-09-18 00:48:35 +0600

    Added comment option

--- resources/views/readerTemp.blade.php
+++ resources/views/readerTemp.blade.php
@@ -8,5 +8,16 @@
 <h1>Currently reading: {{$book->filepath}}</h1>
 <iframe src="/pdfjs/web/viewer.html?file=bookstore/{{$book->filepath}}.pdf" style="border: 0" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
 
+<form method="POST" action="/comments" enctype="multipart/form-data">
+
+{{ csrf_field() }}
+	<div class="form-group">
+            <label class="col-md-3 control-label" name="resume">Write a comment:</label>
+           <!--input  type="text" id="filepath" placeholder="Book Name" name="filepath" class=""/-->
+	<TEXTAREA name="comment_body" class="form-control"></TEXTAREA>
+	<input type="hidden" name="book_id" value="{{ $book->id }}">
+	<input type="submit" class="btn btn-success pull-right">
+</form>
+
 
 @endsection
